22d68c64b693f63f14f72960cf17e9f71a82cee4
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-04 12:33:05 +0100

    Add first exercism challange

---
+++ Exercism/csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class ReverseString
+{
+	public static string Reverse(string input)
+	{
+		return input
+			.Reverse()
+			.Aggregate("", (acc, current) => acc += current);
+	}
+}
