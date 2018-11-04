e97a38589026165800cdf9524fb3c174247a1a13
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-04 12:46:16 +0100

    Do not skip test

--- Exercism/csharp/reverse-string/ReverseStringTest.cs
+++ Exercism/csharp/reverse-string/ReverseStringTest.cs
@@ -4,33 +4,33 @@ using Xunit;
 
 public class ReverseStringTest
 {
-    [Fact]
-    public void An_empty_string()
-    {
-        Assert.Equal("", ReverseString.Reverse(""));
-    }
+	[Fact]
+	public void An_empty_string()
+	{
+		Assert.Equal("", ReverseString.Reverse(""));
+	}
 
-    [Fact(Skip = "Remove to run test")]
-    public void A_word()
-    {
-        Assert.Equal("tobor", ReverseString.Reverse("robot"));
-    }
+	[Fact]
+	public void A_word()
+	{
+		Assert.Equal("tobor", ReverseString.Reverse("robot"));
+	}
 
-    [Fact(Skip = "Remove to run test")]
-    public void A_capitalized_word()
-    {
-        Assert.Equal("nemaR", ReverseString.Reverse("Ramen"));
-    }
+	[Fact]
+	public void A_capitalized_word()
+	{
+		Assert.Equal("nemaR", ReverseString.Reverse("Ramen"));
+	}
 
-    [Fact(Skip = "Remove to run test")]
-    public void A_sentence_with_punctuation()
-    {
-        Assert.Equal("!yrgnuh m'I", ReverseString.Reverse("I'm hungry!"));
-    }
+	[Fact]
+	public void A_sentence_with_punctuation()
+	{
+		Assert.Equal("!yrgnuh m'I", ReverseString.Reverse("I'm hungry!"));
+	}
 
-    [Fact(Skip = "Remove to run test")]
-    public void A_palindrome()
-    {
-        Assert.Equal("racecar", ReverseString.Reverse("racecar"));
-    }
+	[Fact]
+	public void A_palindrome()
+	{
+		Assert.Equal("racecar", ReverseString.Reverse("racecar"));
+	}
 }
