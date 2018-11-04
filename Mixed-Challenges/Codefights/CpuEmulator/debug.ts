f82aed1e224a65511d42552c8fb24fb07440bd7b
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-04 15:49:24 +0100

    Moved Codefights projects to FetzenRndy/CodingChallenges

--- Mixed-Challenges/Codefights/CpuEmulator/debug.ts
+++
@@ -1,8 +0,0 @@
-import { Emulator } from "./CpuEmulator";
-
-const result = Emulator.run(["INV R41", "ADD R42,R41"], {
-	printDebugRegisters: true,
-	printDebugInstructions: true
-});
-
-console.log(result);
