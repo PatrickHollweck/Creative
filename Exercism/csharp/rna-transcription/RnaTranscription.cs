aecaaf98e9df404b14c8c74700f1d53a8a91db9c
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-05 20:51:49 +0100

    Add RNA-Transcription Exercism challenge

---
+++ Exercism/csharp/rna-transcription/RnaTranscription.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Collections.Generic;
+
+public static class RnaTranscription
+{
+	public enum DNA
+	{
+		Adenine = 'A',
+		Cytosine = 'C',
+		Guanine = 'G',
+		Thymine = 'T'
+	}
+
+	public enum RNA
+	{
+		Adenine = 'A',
+		Cytosine = 'C',
+		Guanine = 'G',
+		Uracil = 'U'
+	}
+
+	public static string ToRna(string dna)
+	{
+		return dna
+			.Select(nucleotide => DnaToRnaMappings[(DNA)nucleotide])
+			.Aggregate(string.Empty, (result, nucleotide) => result += (char)nucleotide);
+	}
+
+
+	private static readonly Dictionary<DNA, RNA> DnaToRnaMappings = new Dictionary<DNA, RNA>()
+	{
+		{ DNA.Guanine, RNA.Cytosine },
+		{ DNA.Cytosine, RNA.Guanine },
+		{ DNA.Thymine, RNA.Adenine },
+		{ DNA.Adenine, RNA.Uracil }
+	};
+}
