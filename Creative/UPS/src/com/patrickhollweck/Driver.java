01d6123b76acd9867494e8aeb3a972d99b28ef86
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-12-22 14:42:18 +0100

    Reset Creative/UPS with fresh maven project

--- Creative/UPS/src/com/patrickhollweck/Driver.java
+++
@@ -1,3 +0,0 @@
-package com.patrickhollweck;
-
-public class Driver { }
---
+++ Creative/UPS/src/main/java/com/patrickhollweck/UPS/EntryPoint.java
@@ -0,0 +1,7 @@
+package com.patrickhollweck.UPS;
+
+public class EntryPoint {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
