8531853aa2093613ee27e7542aef267bb1bf7d94
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-03-02 00:39:09 +0100

    Use ConcurrentDictionary instead of Dictionary to allow lock-free async access

--- Creative/StatoBot/StatoBot.Analytics/Statistics.cs
+++ Creative/StatoBot/StatoBot.Analytics/Statistics.cs
@@ -1,47 +1,41 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Concurrent;
 
 namespace StatoBot.Analytics
 {
-	public class Statistics : Dictionary<string, decimal>
-	{
-		private readonly Encoding encoder;
-		private readonly object mutationLock;
+    public class Statistics : ConcurrentDictionary<string, decimal>
+    {
+        private readonly Encoding encoder;
 
-		public Statistics()
-		{
-			encoder = Encoding.GetEncoding(
-				"UTF-8",
-				new EncoderReplacementFallback(string.Empty),
-				new DecoderExceptionFallback()
-			);
+        public Statistics()
+        {
+            encoder = Encoding.GetEncoding(
+                "UTF-8",
+                new EncoderReplacementFallback(string.Empty),
+                new DecoderExceptionFallback()
+            );
+        }
 
-			mutationLock = new object();
-		}
+        public void Increment(string key)
+        {
+            key = SanitizeKey(key);
 
-		public void Increment(string key)
-		{
-			key = SanitizeKey(key);
+            if (!ContainsKey(key))
+            {
+                TryAdd(key, 1);
+            }
+            else
+            {
+                TryGetValue(key, out var count);
+                TryRemove(key, out decimal _);
+                TryAdd(key, count + 1);
+            }
+        }
 
-			lock (mutationLock)
-			{
-				if(!ContainsKey(key))
-				{
-					Add(key, 1);
-				}
-				else
-				{
-					TryGetValue(key, out var count);
-					Remove(key);
-					Add(key, count + 1);
-				}
-			}
-		}
-
-		private string SanitizeKey(string key)
-		{
-			return encoder.GetString(encoder.GetBytes(key));
-		}
-	}
+        private string SanitizeKey(string key)
+        {
+            return encoder.GetString(encoder.GetBytes(key));
+        }
+    }
 }
