fa56a0abd423ddaabe39513b262e13f96322ca5a
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-03-03 19:09:06 +0100

    Add ReportGenerator.Format overload that calls on a instance

--- Creative/StatoBot/StatoBot.Reports/ReportGenerator.cs
+++ Creative/StatoBot/StatoBot.Reports/ReportGenerator.cs
@@ -4,21 +4,21 @@ using StatoBot.Analytics;
 
 namespace StatoBot.Reports
 {
-	public static class ReportGenerator
-	{
-		public static Report ForAnalyzingBot(AnalyzerBot bot)
-		{
-			return new Report(
-				new ReportInput(
-					bot.Analyzer.Statistics,
-					BotInfo.FromBot(bot)
-				)
-			);
-		}
+    public static class ReportGenerator
+    {
+        public static Report FromBot(AnalyzerBot bot)
+        {
+            return new Report(
+                new ReportInput(
+                    bot.Analyzer.Statistics,
+                    BotInfo.FromBot(bot)
+                )
+            );
+        }
 
-		public static Report ForData(ReportInput input)
-		{
-			return new Report(input);
-		}
-	}
+        public static Report FromData(ReportInput input)
+        {
+            return new Report(input);
+        }
+    }
 }
