1607d04d918a75b59c0f0bbe9468cc9ed1518777
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-05-06 20:22:56 +0200

    Solve first codewars challenge

---
+++ Challenges/Codewars/csharp/Challenges/RegexValidatePinCode.cs
@@ -0,0 +1,15 @@
+﻿// https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/csharp
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Challenges
+{
+    public class RegexValidatePinCodeKata
+    {
+        public static bool ValidatePin(string pin)
+        {
+            return new Regex("^([0-9]{4}|[0-9]{6})$").IsMatch(pin);
+        }
+    }
+}
