107f36f1dbdd2c8928ca60e4f1aa71c651262f1e
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-05-26 12:28:35 +0200

    Add "Traffico" project

---
+++ Creative/Traffico/src/main/java/traffico/ProgramApplet.java
@@ -0,0 +1,28 @@
+package traffico;
+
+import processing.core.PApplet;
+import processing.event.KeyEvent;
+
+public class ProgramApplet extends PApplet {
+	@Override
+	public void setup() {
+		super.setup();
+	}
+
+	@Override
+	public void settings() {
+		super.settings();
+		
+		this.size(800, 500);
+	}
+
+	@Override
+	public void draw() {
+		super.draw();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		super.keyPressed(event);
+	}
+}
