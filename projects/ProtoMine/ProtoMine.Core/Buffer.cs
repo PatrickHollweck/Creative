a93b6da53dbebc0a3ccfb826b92033ac43dad43d
Patrick Hollweck <patrick-hollweck@gmx.de>
2022-10-05 00:08:02 +0200

    Add ProtoMine project

---
+++ projects/ProtoMine/ProtoMine.Core/Buffer.cs
@@ -0,0 +1,154 @@
+﻿namespace ProtoMine.Core;
+
+public class Buffer
+{
+	private readonly List<byte> bytes;
+
+	public Buffer()
+	{
+		bytes = new List<byte>();
+	}
+
+	#region Write Functions
+
+	private static IEnumerable<byte> ToBigEndian(IEnumerable<byte> bytes)
+	{
+		if (BitConverter.IsLittleEndian)
+		{
+			bytes = bytes.Reverse();
+		}
+
+		return bytes;
+	}
+
+	private void AddBytes(byte[] inBytes, int? offset)
+	{
+		var convertedBytes = ToBigEndian(inBytes);
+
+		if (offset == null)
+		{
+			bytes.AddRange(convertedBytes);
+		}
+		else
+		{
+			bytes.InsertRange(offset.Value, convertedBytes);
+		}
+	}
+
+	public void WriteInt8(sbyte value, int? offset = null)
+	{
+		AddBytes(new[] { (byte)value }, offset);
+	}
+
+	public void WriteUInt8(byte value, int? offset = null)
+	{
+		AddBytes(new[] { value }, offset);
+	}
+
+	public void WriteInt16(short value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteUInt16(ushort value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteInt32(int value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteUInt32(uint value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteInt64(long value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteUInt64(ulong value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteFloat16(float value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	public void WriteFloat32(double value, int? offset = null)
+	{
+		AddBytes(BitConverter.GetBytes(value), offset);
+	}
+
+	#endregion
+
+	#region Read Functions
+
+	private byte[] Read(int position, int count)
+	{
+		var readBytes = bytes.ToArray()[position..(position + count)];
+
+		return BitConverter.IsLittleEndian ? readBytes.Reverse().ToArray() : readBytes;
+	}
+
+	public sbyte ReadInt8(int position)
+	{
+		return (sbyte)Read(position, 1).First();
+	}
+
+	public byte ReadUInt8(int position)
+	{
+		return Read(position, 1).First();
+	}
+
+	public short ReadInt16(int position)
+	{
+		return BitConverter.ToInt16(Read(position, 2));
+	}
+
+	public ushort ReadUInt16(int position)
+	{
+		return BitConverter.ToUInt16(Read(position, 2));
+	}
+
+	public int ReadInt32(int position)
+	{
+		return BitConverter.ToInt32(Read(position, 4));
+	}
+
+	public uint ReadUInt32(int position)
+	{
+		return BitConverter.ToUInt32(Read(position, 4));
+	}
+
+	public long ReadInt64(int position)
+	{
+		return BitConverter.ToInt64(Read(position, 8));
+	}
+
+	public ulong ReadUInt64(int position)
+	{
+		return BitConverter.ToUInt64(Read(position, 8));
+	}
+
+	public float ReadFloat16(int position)
+	{
+		return BitConverter.Int32BitsToSingle(
+			ReadInt32(position)
+		);
+	}
+
+	public double ReadFloat32(int position)
+	{
+		return BitConverter.Int64BitsToDouble(
+			ReadInt64(position)
+		);
+	}
+
+	#endregion
+}
---
+++ projects/ProtoMine/ProtoMine.Tests/BufferTest.cs
@@ -0,0 +1,183 @@
+using ProtoMine.Core;
+using Xunit;
+
+namespace ProtoMine.Tests;
+
+public class BufferTest
+{
+	[Fact]
+	public void ReadWrite_Int8()
+	{
+		var buffer = new Buffer();
+
+		buffer.WriteInt8(sbyte.MinValue, 0);
+		buffer.WriteInt8(-12, 1);
+		buffer.WriteInt8(0, 2);
+		buffer.WriteInt8(15, 3);
+		buffer.WriteInt8(sbyte.MaxValue, 4);
+
+		Assert.Equal(sbyte.MinValue, buffer.ReadInt8(0));
+		Assert.Equal(-12, buffer.ReadInt8(1));
+		Assert.Equal(0, buffer.ReadInt8(2));
+		Assert.Equal(15, buffer.ReadInt8(3));
+		Assert.Equal(sbyte.MaxValue, buffer.ReadInt8(4));
+	}
+
+	[Fact]
+	public void ReadWrite_UInt8()
+	{
+		var buffer = new Buffer();
+
+		buffer.WriteUInt8(byte.MinValue, 0);
+		buffer.WriteUInt8(1, 1);
+		buffer.WriteUInt8(15, 2);
+		buffer.WriteUInt8(245, 3);
+		buffer.WriteUInt8(byte.MaxValue, 4);
+
+		Assert.Equal(byte.MinValue, buffer.ReadUInt8(0));
+		Assert.Equal(1, buffer.ReadUInt8(1));
+		Assert.Equal(15, buffer.ReadUInt8(2));
+		Assert.Equal(245, buffer.ReadUInt8(3));
+		Assert.Equal(byte.MaxValue, buffer.ReadUInt8(4));
+	}
+
+	[Fact]
+	public void ReadWrite_Int16()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 2;
+
+		buffer.WriteInt16(short.MinValue, 0 * typeByteSize);
+		buffer.WriteInt16(0, 1 * typeByteSize);
+		buffer.WriteInt16(1, 2 * typeByteSize);
+		buffer.WriteInt16(short.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(short.MinValue, buffer.ReadInt16(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadInt16(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadInt16(2 * typeByteSize));
+		Assert.Equal(short.MaxValue, buffer.ReadInt16(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_UInt16()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 2;
+
+		buffer.WriteUInt16(ushort.MinValue, 0 * typeByteSize);
+		buffer.WriteUInt16(0, 1 * typeByteSize);
+		buffer.WriteUInt16(1, 2 * typeByteSize);
+		buffer.WriteUInt16(ushort.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(ushort.MinValue, buffer.ReadUInt16(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadUInt16(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadUInt16(2 * typeByteSize));
+		Assert.Equal(ushort.MaxValue, buffer.ReadUInt16(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_Int32()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 4;
+
+		buffer.WriteInt32(int.MinValue, 0 * typeByteSize);
+		buffer.WriteInt32(0, 1 * typeByteSize);
+		buffer.WriteInt32(1, 2 * typeByteSize);
+		buffer.WriteInt32(int.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(int.MinValue, buffer.ReadInt32(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadInt32(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadInt32(2 * typeByteSize));
+		Assert.Equal(int.MaxValue, buffer.ReadInt32(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_UInt32()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 4;
+
+		buffer.WriteUInt32(uint.MinValue, 0 * typeByteSize);
+		buffer.WriteUInt32(0, 1 * typeByteSize);
+		buffer.WriteUInt32(1, 2 * typeByteSize);
+		buffer.WriteUInt32(uint.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(uint.MinValue, buffer.ReadUInt32(0 * typeByteSize));
+		Assert.Equal((uint)0, buffer.ReadUInt32(1 * typeByteSize));
+		Assert.Equal((uint)1, buffer.ReadUInt32(2 * typeByteSize));
+		Assert.Equal(uint.MaxValue, buffer.ReadUInt32(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_Int64()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 8;
+
+		buffer.WriteInt64(long.MinValue, 0 * typeByteSize);
+		buffer.WriteInt64(0, 1 * typeByteSize);
+		buffer.WriteInt64(1, 2 * typeByteSize);
+		buffer.WriteInt64(long.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(long.MinValue, buffer.ReadInt64(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadInt64(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadInt64(2 * typeByteSize));
+		Assert.Equal(long.MaxValue, buffer.ReadInt64(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_UInt64()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 8;
+
+		buffer.WriteUInt64(ulong.MinValue, 0 * typeByteSize);
+		buffer.WriteUInt64(0, 1 * typeByteSize);
+		buffer.WriteUInt64(1, 2 * typeByteSize);
+		buffer.WriteUInt64(ulong.MaxValue, 3 * typeByteSize);
+
+		Assert.Equal(ulong.MinValue, buffer.ReadUInt64(0 * typeByteSize));
+		Assert.Equal((ulong)0, buffer.ReadUInt64(1 * typeByteSize));
+		Assert.Equal((ulong)1, buffer.ReadUInt64(2 * typeByteSize));
+		Assert.Equal(ulong.MaxValue, buffer.ReadUInt64(3 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_Float16()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 4;
+
+		buffer.WriteFloat16(float.MinValue, 0 * typeByteSize);
+		buffer.WriteFloat16(0, 1 * typeByteSize);
+		buffer.WriteFloat16(1, 2 * typeByteSize);
+		buffer.WriteFloat16(float.MaxValue, 3 * typeByteSize);
+		buffer.WriteFloat16(1.234f, 4 * typeByteSize);
+
+		Assert.Equal(float.MinValue, buffer.ReadFloat16(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadFloat16(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadFloat16(2 * typeByteSize));
+		Assert.Equal(float.MaxValue, buffer.ReadFloat16(3 * typeByteSize));
+		Assert.Equal(1.234f, buffer.ReadFloat16(4 * typeByteSize));
+	}
+
+	[Fact]
+	public void ReadWrite_Float32()
+	{
+		var buffer = new Buffer();
+		var typeByteSize = 8;
+
+		buffer.WriteFloat32(double.MinValue, 0 * typeByteSize);
+		buffer.WriteFloat32(0, 1 * typeByteSize);
+		buffer.WriteFloat32(1, 2 * typeByteSize);
+		buffer.WriteFloat32(double.MaxValue, 3 * typeByteSize);
+		buffer.WriteFloat32(1.234, 4 * typeByteSize);
+
+		Assert.Equal(double.MinValue, buffer.ReadFloat32(0 * typeByteSize));
+		Assert.Equal(0, buffer.ReadFloat32(1 * typeByteSize));
+		Assert.Equal(1, buffer.ReadFloat32(2 * typeByteSize));
+		Assert.Equal(double.MaxValue, buffer.ReadFloat32(3 * typeByteSize));
+		Assert.Equal(1.234, buffer.ReadFloat32(4 * typeByteSize));
+	}
+}
