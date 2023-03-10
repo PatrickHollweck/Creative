72a3a6449509dd1e5e336bb077d4a79762ba2b87
Patrick Hollweck <patrick-hollweck@gmx.de>
2023-03-10 01:13:27 +0100

    Refactor src/main into examples

---
+++ projects/MinePack/example/status-request.ts
@@ -0,0 +1,32 @@
+import { MinecraftClient } from "../lib/MinecraftClient.js";
+import { ProtocolVersion } from "../lib/ProtocolVersion.js";
+
+import { waitForPacket } from "../lib/protocol/packets/util.js";
+import { attachDisplayHandlers } from "./util.js";
+
+import * as packets from "../lib/protocol/packets/v761/index.js";
+
+const client = new MinecraftClient(ProtocolVersion.v761);
+
+const host = "minecraft.patrickhollweck.de";
+const port = 25565;
+
+attachDisplayHandlers(client);
+
+await client.connect(host, port);
+
+const handshakePacket = new packets.HandshakePacket();
+handshakePacket.nextState = 1;
+handshakePacket.serverPort = port;
+handshakePacket.serverAddress = host;
+handshakePacket.protocolVersion = 761;
+
+await client.writer.sendPacket(handshakePacket);
+await client.writer.sendPacket(new packets.StatusRequestPacket());
+
+await waitForPacket(client.reader, packets.StatusResponsePacket);
+
+const pingRequestPacket = new packets.PingRequestPacket();
+pingRequestPacket.payload = Date.now().valueOf();
+
+await client.writer.sendPacket(pingRequestPacket);
---
+++ projects/MinePack/example/util.ts
@@ -0,0 +1,66 @@
+import c from "chalk";
+
+import { MinecraftClient } from "../lib/MinecraftClient.js";
+import { PacketSerializer } from "../lib/protocol/PacketSerializer.js";
+
+function displayHeader(text: string, color: (text: string) => string) {
+	console.log(color(text.padEnd(70)));
+}
+
+export function attachDisplayHandlers(client: MinecraftClient) {
+	client.reader.events.on("packet", (packet, type) => {
+		displayHeader(
+			`RECEIVED PACKET (${type.name})`,
+			c.bgGreenBright.black.bold
+		);
+
+		const serialized = JSON.stringify(
+			packet,
+			(key, value) => {
+				return typeof value === "bigint" ? value.toString() : value;
+			},
+			4
+		);
+
+		console.log(c.green(serialized));
+	});
+
+	client.writer.events.on("sendComplete", (packet, buffer) => {
+		const type = PacketSerializer.getPacketType(packet);
+
+		displayHeader(`PACKET SENT (${type.name})`, c.bgBlueBright.black.bold);
+		console.log(c.blue("## AS HEX:", buffer.toString("hex")));
+		console.log(c.blue(JSON.stringify(packet, null, 4)));
+	});
+
+	client.writer.events.on("sendError", (error) => {
+		displayHeader("PACKET SEND ERROR", c.bgRed.black.bold);
+		console.log(c.red(JSON.stringify(error, null, 4)));
+	});
+
+	client.socket.on("connect", () => {
+		displayHeader("SOCKET CONNECTED", c.bgYellow.black);
+	});
+
+	client.socket.on("data", (chunk: Buffer) => {
+		displayHeader("SOCKET DATA RECEIVED", c.bgGray.black.bold);
+		console.log(c.gray("## AS HEX:", chunk.toString("hex")));
+		console.log(c.gray("## AS UTF:", chunk.toString("utf-8")));
+	});
+
+	client.socket.on("error", (error) => {
+		displayHeader("SOCKET ERROR", c.bgRed.black.bold);
+		console.error(c.red(JSON.stringify(error, null, 4)));
+	});
+
+	client.socket.on("end", (reason: unknown) => {
+		displayHeader("SOCKET END", c.bgYellow.black);
+		console.log(c.yellow("Reason:", JSON.stringify(reason)));
+
+		client.disconnect();
+	});
+
+	client.socket.on("close", () =>
+		displayHeader("SOCKET CLOSED (TERMINATED)", c.bgYellow.black)
+	);
+}
